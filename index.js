88d15b0e72d6f4f1ef517ec2ae3eb71101132b61
Ubuntu <itua@pennappsubuntu.yoqf24dows4e3kzuofrcp0n5lh.dx.internal.cloudapp.net>
2015-09-06 01:44:13 +0000

    server

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
