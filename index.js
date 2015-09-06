d643e7a6bfa4a63850073977685cbbb8a19450c4
Itua Ijagbone <ituaijagbone@Ituas-MacBook-Pro.local>
2015-09-06 01:16:05 -0400

    Integration testing was good. Need to work on storage and mongodb

--- index.js
+++ index.js
@@ -3,10 +3,38 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var slides = ["pin1.jpg", "pin2.jpg", "pin3.jpg", "pin4.jpg", "pin5.jpg"]
+var slides = ["pin1.jpg", "pin2.jpg", "pin3.jpg", "pin4.jpg", "pin5.jpg"];
+var numberOfSlides = 1;
+
 app.use(express.static('public'));
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/presentations', function(req, res, next) {
+  var presentations = []
+  for (i = 0; i < numberOfSlides; i++) {
+    var presentation = {}
+    presentation["title"] = "Testing Hack";
+    presentation["thumbnail"] = "pin1.jpg";
+    presentation["id"] = "1";
+    presentations.push(presentation);
+  }
+  res.json({"results": presentations})
+});
+
+app.get('/slides', function(req, res, next) {
+  var presentationId = parseInt(req["query"]["pid"]);
+  var slidesR = []
+  console.log(slides.length)
+  for (i = 0; i < slides.length; i++) {
+    var slide = {}
+    slide["title"] = slides[i];
+    slide["posterUrl"] = slides[i];
+    slidesR.push(slide);
+    console.log("here")
+  }
+  res.json({"results": slidesR})
 });
 
 io.on('connection', function(socket){
